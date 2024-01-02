d6e28c3464b726d5e2ac1b75829d62b8f3401148
sameer <sameermuslim777@gmail.com>
2024-01-02 22:12:07 +0330

    add Testimonials show dialog features

--- index.js
+++ index.js
@@ -1,8 +1,56 @@
 const showContactsBtn = document.getElementById("show-contacts");
 const left_side = document.getElementById("left-side");
+const overlay_panel = document.getElementById("overlay");
+const dialog_panel = document.getElementById("full-screen-dialog");
+const dialog_close_btn = document.getElementById("dialog-close-btn");
 
+document.addEventListener("keydown", function (event) {
+  // Check if the pressed key is the "Escape" key (key code 27)
+  if (event.key === "Escape" || event.keyCode === 27) {
+    if (dialog_panel && dialog_panel.classList.contains("active")) {
+      // Your code to handle the "Escape" key press goes here
+      dialog_panel.classList.remove("active");
+      overlay_panel.classList.remove("active");
+    }
+  }
+});
+
+dialog_close_btn.addEventListener("click", dialogToggle);
+overlay_panel.addEventListener("click", dialogToggle);
+function dialogToggle() {
+  dialog_panel.classList.toggle("active");
+  overlay_panel.classList.toggle("active");
+}
 showContactsBtn.addEventListener("click", function () {
   left_side.classList.toggle("active");
+});
+
+// get testimonials  items
+const testimonialsItem = document.querySelectorAll(".card-item.team");
+
+// Add a click listener to each list item
+testimonialsItem.forEach(function (item) {
+  item.addEventListener("click", function () {
+    // Your click event code goes here
+    dialogToggle();
+    const imageSrc = item.querySelector(".photo img").src;
+    const cardItemTitle = item.querySelector(".card-item-title").textContent;
+    const itemText = item.querySelector(".item-text").textContent;
+    const itemDate = item.querySelector(".item-test-mo-date").textContent;
+
+    // Get references to dialog elements
+    const dialogPersonImg = document.querySelector(".dialog-person-img");
+    const dialogPersonName = document.querySelector(".dialog-person-name");
+    const dialogText = document.querySelector(".dialog-text");
+    const dialogDate = document.querySelector(".dialog-date");
+
+    // Set values in the dialog
+    dialogPersonImg.src = imageSrc;
+    dialogPersonName.textContent = cardItemTitle;
+    dialogText.textContent = itemText;
+    dialogDate.textContent = itemDate;
+    console.log("Clicked on:", imageSrc + cardItemTitle + itemText); // Example: Log the text content of the clicked item
+  });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
